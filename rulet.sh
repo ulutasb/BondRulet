94efb5afeddb0d0f4727bf6c19357d057a05d599
ulutasb <81263359+ulutasb@users.noreply.github.com>
2024-08-05 19:07:44 +0300

    Update and rename v9.sh to rulet.sh

--- rulet.sh
+++ rulet.sh
@@ -15,7 +15,14 @@ while IFS='=' read -r key value; do
         DATA_GW) DATA_GW="$value" ;;
         DATA_IP) DATA_IP="$value" ;;
         DATA_LACP) DATA_LACP="$value" ;;
-    
+        BACKUP_BOND_NAME) BACKUP_BOND_NAME="$value" ;;
+        BACKUP__BW) BACKUP__BW="$value" ;;
+        CREATE_BACKUP_BOND) CREATE_BACKUP_BOND="$value" ;;
+       ## BACKUP__GW)BACKUP__GW="$value" ;;
+        BACKUP__IP) BACKUP_IP="$value" ;;
+       BACKUP__LACP) BACKUP__LACP="$value" ;;
+
+
     esac
 done < "$CONFIG_FILE"
 
@@ -95,20 +102,84 @@ test_and_cleanup_bond() {
     fi
 }
 
+
+# BACKUP Bond'u test etme ve bant genişliği tutmazsa silme fonksiyonu
+test_and_cleanup_backup_bond() {
+    local bond_name=$1
+    local expected_bw=$2
+    shift 2
+    local nics=("$@")
+
+    # 5 saniye bekle
+    sleep 7
+
+
+
+    # Etthool ile bant genişliğini kontrol et
+    local bw=$(ethtool "$bond_name" | grep "Speed" | awk '{print $2}' | sed 's/Mb\/s//')
+
+    if [[ "$bw" == "$expected_bw" ]]; then
+        echo "$bond_name bondu başarıyla oluşturuldu. Bant genişliği doğru: $bw Mb/s"
+        return 0
+    else
+        echo "Bond $bond_name başarısız, beklenen bant genişliği: ${expected_bw} Mb/s, mevcut: $bw Mb/s"
+
+        # Bond ve slave'leri sil
+        nmcli con down "$bond_name"
+        nmcli con delete "$bond_name"
+        for nic in "${nics[@]}"; do
+            nmcli con delete $(nmcli -t -f UUID,DEVICE,NAME con | grep "$nic" | cut -d: -f1) && echo "$nic'e tanımlanmis slave siliniyor."
+        done
+        return 1
+    fi
+}
+
+
 # Data Bond'u oluştur ve test et
 for ((i = 0; i < ${#nic_array[@]} - 1; i++)); do
     for ((j = i + 1; j < ${#nic_array[@]}; j++)); do
         data_nics=("${nic_array[i]}" "${nic_array[j]}")
-        echo "Data Bond deniyor: ${data_nics[*]}"
+        echo "DATA Bond'u deneniyor: ${data_nics[*]}"
 
         create_bond "$BOND_NAME" "$DATA_IP" "$DATA_GW" "$DATA_LACP" "${data_nics[@]}"
 
         if test_and_cleanup_bond "$BOND_NAME" "$DATA_BW" "${data_nics[@]}"; then
             echo "Başarılı Data Bond konfigürasyonu bulundu: ${data_nics[*]}"
-            exit 0
+
+
+            backup_remaining_nics=()
+            for nic in "${nic_array[@]}"; do
+                if [[  " ${data_nics[@]} " != " ${nic} " ]]; then
+                    backup_remaining_nics+=("$nic")
+                    echo "nic listesi guncelleniyor"
+                fi
+
+            done
+
+            #if [ "$CREATE_BACKUP_BOND" == "evet" ]; then
+              for ((i = 0; i < ${#backup_remaining_nics[@]} - 1; i++)); do
+                   for ((j = i + 1; j < ${#backup_remaining_nics[@]}; j++)); do
+                      data_nics=("${nic_array[i]}" "${nic_array[j]}")
+                     echo "BACKUP Bond'u deneniyor: ${data_nics[*]}"
+
+                     create_bond "$BACKUP_BOND_NAME" "$BACKUP_IP" "$BACKUP__GW" "$BACKUP__LACP" "${data_nics[@]}"
+
+                     if test_and_cleanup_backup_bond "$BACKUP_BOND_NAME" "$BACKUP_BW" "${data_nics[@]}"; then
+                          echo "Başarılı BACKUP Bond konfigürasyonu bulundu: ${data_nics[*]}"
+                          exit 0
+
+                     else
+                      echo "DATA BONDU BASARIYLA YAPILANDIRILDI FAKAT BACKUP YAPILANDIRILIRKEN HATA ALINDI!!"
+                      exit 1
+                     fi
+                   done
+              done
+           # fi
+
         fi
+
     done
 done
 
-echo "Başarılı Data Bond konfigürasyonu bulunamadı."
-exit 1
+
+
